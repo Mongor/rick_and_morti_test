3ae2596ab878a8e9f96d512a7a7a8cdcadf60639
Mongor <mitrobaz@gmail.com>
2021-03-12 00:11:42 +0200

    add new style

--- src/components/MyWatchList/WatchItem/WatchItem.js
+++ src/components/MyWatchList/WatchItem/WatchItem.js
@@ -3,7 +3,14 @@ import React from 'react';
 import './WatchItem.scss';
 
 const WatchItem = (props) => {
-	return <div className='watch_item'></div>;
+	return (
+		<div className='watch_item'>
+			<input type='checkbox' />
+			<span>name</span>
+			<span>some text</span>
+			<button>x</button>
+		</div>
+	);
 };
 
 export default WatchItem;
